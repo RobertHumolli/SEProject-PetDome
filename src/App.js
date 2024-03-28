8b14bb053ce57d831d2e7b2b46eb22d487002446
R0bertRobz <106174092+R0bertRobz@users.noreply.github.com>
2024-03-28 10:47:47 +0000

    recent

--- src/App.js
+++ src/App.js
@@ -6,10 +6,14 @@ import AboutPage from './Components/AboutPage';
 function App() {
 
   const backgroundImageStyle = {
-    backgroundImage: 'url(/Dog.jpg)',
-    backgroundSize: 'cover',
+    backgroundImage: 'url(/Dog.jpg)', 
+    backgroundSize: 'cover', // Change to 'cover' to fill the container
+    backgroundPosition: 'center center',
     backgroundRepeat: 'no-repeat',
+    minHeight: '100vh', // Adjust as necessary
+    width: '100vw', // Ensure the width is also covering the full viewport
   };
+  
 
   return (
     <Router>
