cddb7ee5c69b3ed5fe08f5c0134f21ed964773cf
R0bertRobz <106174092+R0bertRobz@users.noreply.github.com>
2024-03-28 09:58:31 +0000

    Update

---
+++ src/Components/AboutPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './AboutPage.css';
+
+const AboutPage = () => {
+  return (
+    <div className="about-container">
+      <h1>About Us</h1>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam feugiat lacus eu ante scelerisque, et commodo nulla convallis. Proin congue felis non ligula dapibus, ac laoreet neque interdum. Fusce ac dapibus turpis. Integer lobortis mi quis sollicitudin aliquam.</p>
+    </div>
+  );
+}
+
+export default AboutPage;
---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,34 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom'; // Import the Link component
+import "./Navbar.css";
+
+function Navbar(props) {
+    const [active, setActive] = useState('nav__menu');
+    const [toggleIcon, setToggleIcon] = useState("nav__toggler"); // Corrected state value
+
+    const navToggle = () => {
+        setActive(active === 'nav__menu' ? 'nav__menu nav__active' : 'nav__menu');
+        setToggleIcon(toggleIcon === 'nav__toggler' ? 'nav__toggler toggle' : 'nav__toggler'); // Corrected state value
+    }
+
+    return (
+        <nav className='nav'>
+            <Link to="/" className="nav__brand">PetDome</Link> {/* Use Link component for the brand */}
+            <ul className={active}>
+                {/* Use Link component with the 'to' prop set to the path of the AboutPage */}
+                <li className="nav__item"><Link to="/about" className="nav__link">About</Link></li>
+                <li className="nav__item"><a href="#" className="nav__link">Qualifications</a></li>
+                <li className="nav__item"><a href="#" className="nav__link">Login</a></li>
+                <li className="nav__item"><a href="#" className="nav__link">Register</a></li>
+            </ul>
+            <div onClick={navToggle} className={toggleIcon}>
+                <div className="line1"></div>
+                <div className="line2"></div>
+                <div className="line3"></div>
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar;
+
