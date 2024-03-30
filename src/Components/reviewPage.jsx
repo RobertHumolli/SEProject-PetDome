91d025ac1a446fa83e310da11f7749972198d30d
Gift Chinaeme Akor <g.akor@se22.qmul.ac.uk>
2024-03-30 18:13:01 +0000

    Update reviewPage.jsx

--- src/Components/reviewPage.jsx
+++ src/Components/reviewPage.jsx
@@ -7,7 +7,7 @@ const reviewPage = () => {
     backgroundImage: 'url(/Dog.jpg)', 
     backgroundSize: 'cover', // Change to 'cover' to fill the container
     backgroundPosition: 'center center',
-    backgroundRepeat: 'no-repeat',
+    backgroundRepeat: 'no-repeat', //background position
     minHeight: '100vh', // Adjust as necessary
     width: '100vw', // Ensure the width is also covering the full viewport
   };
