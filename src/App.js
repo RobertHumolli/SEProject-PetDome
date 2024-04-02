adf0bdd5cbf58a2b7bee1790828b16888df99f65
JosephM0 <jmtalo31@gmail.com>
2024-04-02 03:33:14 +0100

    added login and register page, working on Profile creation page

--- src/App.js
+++ src/App.js
@@ -2,14 +2,20 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './Components/Navbar';
 import AboutPage from './Components/AboutPage';
+import Login from './Login';
+import Register from './Register';
 
 function App() {
 
   const backgroundImageStyle = {
-    backgroundImage: 'url(/Dog.jpg)',
-    backgroundSize: 'cover',
+    backgroundImage: 'url(/Dog.jpg)', 
+    backgroundSize: 'cover', // Change to 'cover' to fill the container
+    backgroundPosition: 'center center',
     backgroundRepeat: 'no-repeat',
+    minHeight: '100vh', // Adjust as necessary
+    width: '100vw', // Ensure the width is also covering the full viewport
   };
+  
 
   return (
     <Router>
@@ -17,6 +23,8 @@ function App() {
         <Navbar />
         <Routes>
           <Route path="/about" element={<AboutPage />} /> {/* 'component' prop is replaced with 'element' */}
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
         </Routes>
       </div>
     </Router>
