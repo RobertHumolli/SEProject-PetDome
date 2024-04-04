e7da65ff9f8230e6ae805635c13660b4ad925b58
JosephM0 <jmtalo31@gmail.com>
2024-04-04 12:28:21 +0100

    made navbar to Navbar

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
-import Navbar from './Components/navbar';
+import Navbar from './Components/Navbar';
 import AboutPage from './Components/AboutPage';
 
 import reviewPage from './pages/reviewPage';
