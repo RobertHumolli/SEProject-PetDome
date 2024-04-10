7294d524a96b0f2acc262146d5817d4cd611ab24
MalcolmAsoegwu <120408528+MalcolmAsoegwu@users.noreply.github.com>
2024-04-10 14:45:49 +0100

    Final

--- src/Components/QualPage.jsx
+++ src/Components/QualPage.jsx
@@ -15,23 +15,32 @@ function Qualifications() {
         setFiles(updatedFiles);
     };
 
+    const handleUpload = () => {
+        // Perform upload logic here
+        // Display thank you message
+        alert("Thank you for uploading your qualifications!");
+    };
+
     return (
         <div className="qual-container">
             <h1>Please upload your qualifications</h1>
             <input type="file" multiple onChange={file_upload} />
+            
             {files.length > 0 ? (
                 <div>
-                    <p class ="file_desc1">Thank you for uploading the following files:</p>
-                    <p class = "file_desc2">Click on the 'Remove' button to remove a file. Files not removed are automatically uploaded.</p>
+                    <p class="file_desc1">Thank you for uploading the following files:</p>
+                    <p class="file_desc2">Click on the 'Remove' button to remove a file. Press upload button once ready.</p>
                     {files.map((file, index) => (
                         <div key={index}>
                             <p className='file_name'>{file.name}</p>
                             <button onClick={() => file_remove(index)}>Remove</button>
                         </div>
                     ))}
+                    <button class='upload' onClick={handleUpload}>Upload</button>
                 </div>
             ) : (
-                <p class = "file_desc1">No files uploaded yet.</p>
+                <p class="file_desc1">No files uploaded yet.</p>
+                
             )}
         </div>
     );
