66dfdcf20b8d727fbce0aac1244aa8d4ce008624
MalcolmAsoegwu <120408528+MalcolmAsoegwu@users.noreply.github.com>
2024-04-10 13:20:27 +0100

    Improved Structure of QualPage.jsx and QualPage.css

--- src/Components/QualPage.jsx
+++ src/Components/QualPage.jsx
@@ -6,12 +6,12 @@ function Qualifications() {
 
     const file_upload = (event) => {
         const uploadedFiles = Array.from(event.target.files);
-        setFiles([...files, ...uploadedFiles]); // ... is used to concatenate new files to array of exisiting files stored in variable
+        setFiles([...files, ...uploadedFiles]);
     };
 
     const file_remove = (index) => {
         const updatedFiles = [...files]; 
-        updatedFiles.splice(index, 1); // remove the file at the specified index
+        updatedFiles.splice(index, 1);
         setFiles(updatedFiles);
     };
 
@@ -19,12 +19,20 @@ function Qualifications() {
         <div className="qual-container">
             <h1>Please upload your qualifications</h1>
             <input type="file" multiple onChange={file_upload} />
-            {files.map((file, index) => (
-                <div key={index}>
-                    <p>{file.name}</p>
-                    <button onClick={() => file_remove(index)}>Remove</button> {/* when remove button is pressed, file_remove function is used to remove selected file */}
+            {files.length > 0 ? (
+                <div>
+                    <p>Thank you for uploading the following files:</p>
+                    <p>Click on the 'Remove' button to remove a file.</p>
+                    {files.map((file, index) => (
+                        <div key={index}>
+                            <p>{file.name}</p>
+                            <button onClick={() => file_remove(index)}>Remove</button>
+                        </div>
+                    ))}
                 </div>
-            ))}
+            ) : (
+                <p>No files uploaded yet.</p>
+            )}
         </div>
     );
 }
